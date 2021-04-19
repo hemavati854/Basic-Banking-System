86ba7cda914e70957d20462e16d2f92635decec9
Hemavati <82763301+hemavati854@users.noreply.github.com>
2021-04-19 14:02:46 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-	//$conn = mysqli_connect('shareddb-x.hosting.stackcp.net','shivangi','12345678@','malyaraaj-313537e38d');
+	
 
 	$conn = new mysqli('localhost','root','','bank');
 	if(!$conn){
